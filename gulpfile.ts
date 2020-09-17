c565a2db10ac27bbb9da18d90498a53eaf1e8e36
Romildo Júnior <jrmmendes@outlook.com>
2020-09-17 20:40:47 -0300

    Add gulp-nodemon

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,6 +2,7 @@ import del from 'del';
 import gulp, { dest, series, src } from 'gulp';
 import eslint from 'gulp-eslint';
 import tsc from 'gulp-typescript';
+import nodemon from 'gulp-nodemon';
 
 const tsProject = tsc.createProject('tsconfig.json', { noImplicitAny: true });
 
@@ -16,13 +17,23 @@ export function lint() {
 }
 
 export function compile() {
-  return tsProject.src()
+  return src('./src/**/*.ts')
     .pipe(tsProject())
     .pipe(dest('dist'));
 }
 
 export function watch() {
   return gulp.watch('src/**/*.ts', compile);
+}
+
+export function startNodemon() {
+  compile();
+  return nodemon({
+    script: 'dist/main.js',
+    ext: 'ts',
+    watch: ['./src/'],
+    tasks: ['compile'],
+  });
 }
 
 export const build = series(clean, lint, compile);
