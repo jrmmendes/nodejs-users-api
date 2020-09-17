c5748cea1bfd46287442f564a8eece3381ab9cca
Romildo Júnior <jrmmendes@outlook.com>
2020-09-17 20:16:09 -0300

    Add build related gulp tasks (clean, lint, watch and compile)

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,16 +1,28 @@
 import del from 'del';
+import gulp, { dest, series, src } from 'gulp';
 import eslint from 'gulp-eslint';
-import { src } from 'gulp';
+import tsc from 'gulp-typescript';
 
-export function clean(callback: Function) {
-  del(['./dist/**']);
-  callback();
+const tsProject = tsc.createProject('tsconfig.json', { noImplicitAny: true });
+
+export function clean() {
+  return del(['./dist/**']);
 };
 
-export function lint(callback: Function) {
-  src('src/**/*.ts')
+export function lint() {
+  return src('src/**/*.ts')
     .pipe(eslint())
     .pipe(eslint.format());
-
-  callback();
 }
+
+export function compile() {
+  return tsProject.src()
+    .pipe(tsProject())
+    .pipe(dest('dist'));
+}
+
+export function watch() {
+  return gulp.watch('src/**/*.ts', compile);
+}
+
+export const build = series(clean, lint, compile);
