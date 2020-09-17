f2538622d430ef34d5594bc5b607a4e80336dddb
Romildo Júnior <jrmmendes@outlook.com>
2020-09-17 19:49:28 -0300

    Add lint task to gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,16 @@
 import del from 'del';
+import eslint from 'gulp-eslint';
+import { src } from 'gulp';
 
 export function clean(callback: Function) {
   del(['./dist/**']);
   callback();
 };
+
+export function lint(callback: Function) {
+  src('src/**/*.ts')
+    .pipe(eslint())
+    .pipe(eslint.format());
+
+  callback();
+}
