a0549fc25b6404e35faf9133696e3b43a8fb69d3
Romildo Júnior <jrmmendes@outlook.com>
2020-09-21 08:59:35 -0300

    Add test example with mock

--- src/example/example.service.spec.ts
+++ src/example/example.service.spec.ts
@@ -1,8 +1,27 @@
+import { ExampleRepository } from "./example.repository";
+import { ExampleService } from "./example.service";
+
 describe('Example Unit Tests', () => {
+  let exampleRepositoryMock: ExampleRepository;
+  let service: ExampleService;
+
+  beforeAll(() => {
+    exampleRepositoryMock = {
+      create: jest.fn(),
+      save: jest.fn(),
+      findAll: jest.fn(),
+      findById: jest.fn(),
+    } as any;
+    service = new ExampleService(exampleRepositoryMock);
+  });
   it('Should return hello word', async () => {
-    // const service = new ExampleService();
-    // const message = await service.getHelloMessage();
-    // expect(message).toBe('hello world again');
+    const message = await service.getHelloMessage();
+    expect(message).toBe('hello world again');
     expect(true).toBe(true);
+  });
+
+  it('Should create examples', async ()=> {
+    service.registerExample();
+    expect(exampleRepositoryMock.create).toBeCalled();
   });
 })
