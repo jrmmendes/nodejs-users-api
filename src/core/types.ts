1905a47aa3db94ebb436098a938dfa46863e1935
Romildo Júnior <jrmmendes@outlook.com>
2020-09-21 10:28:27 -0300

    Add symbols for users repository and service

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,5 +1,7 @@
 export const Types = {
   ExampleService: Symbol.for('ExampleService'),
+  UserService: Symbol.for('UserService'),
   DatabaseClient: Symbol.for('DatabaseClient'),
   ExampleRepository: Symbol.for('ExampleRepository'),
+  UserRepository: Symbol.for('UserRepository'),
 };
