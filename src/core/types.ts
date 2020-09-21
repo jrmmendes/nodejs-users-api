d12677b1ba8c624ac11a1d703d91d2e47326d6b8
Romildo Júnior <jrmmendes@outlook.com>
2020-09-21 11:15:51 -0300

    Remove example module

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,7 +1,5 @@
 export const Types = {
-  ExampleService: Symbol.for('ExampleService'),
   UserService: Symbol.for('UserService'),
   DatabaseClient: Symbol.for('DatabaseClient'),
-  ExampleRepository: Symbol.for('ExampleRepository'),
   UserRepository: Symbol.for('UserRepository'),
 };
