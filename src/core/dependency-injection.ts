4d0a088dadf3cec6ff44a9a05c4f57b48ea52e68
Romildo Júnior <jrmmendes@outlook.com>
2020-09-20 22:27:31 -0300

    Add ExampleRepository to DI container

--- src/core/dependency-injection.ts
+++ src/core/dependency-injection.ts
@@ -5,12 +5,16 @@ import { Types } from '~/core/types';
 import { ExampleService } from '~/example/example.service';
 
 import '../example/example.controller';
+import { ExampleRepository } from '~/example/example.repository';
 
 export const getDependencyContainer = () => {
   const container = new Container();
-  container
-    .bind<ExampleService>(Types.ExampleService)
+
+  container.bind<ExampleService>(Types.ExampleService)
     .to(ExampleService);
+
+  container.bind<ExampleRepository>(Types.ExampleRepository)
+    .to(ExampleRepository).inSingletonScope();
 
   return container;
 }
