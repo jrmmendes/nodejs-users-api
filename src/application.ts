5de7d51144dad13ae49ab5f7e8f1b5d4674324d6
Romildo Júnior <jrmmendes@outlook.com>
2020-09-22 11:49:04 -0300

    Add express-bearer-token to dependencies

--- src/application.ts
+++ src/application.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import { Container } from 'inversify';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import morgan from 'morgan';
+import bearerToken from 'express-bearer-token';
 
 export const bootstrap = (container: Container): Application => {
   const app = new InversifyExpressServer(container);
@@ -13,6 +14,7 @@ export const bootstrap = (container: Container): Application => {
     app.use(helmet());
     app.use(morgan('dev'));
     app.use(express.json());
+    app.use(bearerToken({ reqKey: 'token' }));
   });
 
   return app.build();
